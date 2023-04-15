ce158dab5dafe36aec50fccaad23165c39caf327
Elias Fernandez <elaine.fernandez@obf.ateneo.edu>
2023-04-15 14:29:28 +0800

    Modified to show long and lat values in website
    views.py:
    - Added 'return' statement to get_ip_geolocation_data
    - Added 'context' in gen_qr
    
    index.html:
    - Added lat and long variables

--- geotaggingapp/adminclient/views.py
+++ geotaggingapp/adminclient/views.py
@@ -15,9 +15,10 @@ api_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + api_key
 
 long = ""
 lat = ""
+ip = ""
 
 def get_ip_geolocation_data(ip_address, long, lat):    
-
+    
     response = requests.get(api_url) 
     # okay we got the longitude latitude information but its not showing in
     # the app instead its printed in the terminal
@@ -27,14 +28,16 @@ def get_ip_geolocation_data(ip_address, long, lat):
     data = json.loads(stack)
     long = data['longitude']
     lat = data['latitude']
-    print(long)
-    print(lat)
+    
+    return long, lat
 
 
 data=None
 def qr_gen(request):
     global data
+    
     if request.method=="POST":
+        ip = request.POST['data']
         data=request.POST['data']
         img=make(data)
         img.save("static/image/test.png")
@@ -47,8 +50,20 @@ def qr_gen(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     
-    get_ip_geolocation_data(ip, long, lat)
-    return render(request,"index.html",{'data':data})
+    getCoords = get_ip_geolocation_data(ip, long, lat)
+    x = getCoords[0]
+    y = getCoords[1]
+        
+    print(x)
+    print(y)
+        
+    context = {
+        'long' : x,
+        'lat' : y,
+        'data' : data,
+    }
+    
+    return render(request,"index.html", context)
 
 def adminclientview(request):
 
@@ -66,21 +81,3 @@ def adminclientview(request):
 
 def index(request):
     return render(request,'index.html')
-
-def get_location(request):
-     
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    
-    context = {
-        'longitude' : long,
-        'latitude' : lat,
-    }
-    
-    get_ip_geolocation_data(ip, long, lat)
-    
-    return render(request,"index.html",context)
