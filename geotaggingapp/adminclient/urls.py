54f80611c08a7a819914bbe6072e9880319827df
Mel1o <101039200+Mel1o@users.noreply.github.com>
2023-04-15 12:22:28 +0800

    Made background image and changed style
    Making the html drippier

--- geotaggingapp/adminclient/urls.py
+++ geotaggingapp/adminclient/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('', views.adminclientview, name='adminclientview'),
     path('', views.qr_gen, name='qr_gen'),
     path('send', views.qr_gen, name='qr_gen'),
+    path('',views.index),
 ]
 
 app_name = "adminclient"
--- geotaggingapp/adminclient/views.py
+++ geotaggingapp/adminclient/views.py
@@ -1,9 +1,9 @@
-# Create your views here.
-
 from django.shortcuts import render, HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 from qrcode import *
+import requests
+import json
 
 # THIS IS OUR API KEY DO NOT LOSE IT
 # if we lose it i can just go get it again but yea
@@ -13,15 +13,23 @@ api_key = '69085a26250343e6bb8936995e02e4c3';
 
 api_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + api_key
 
+long = ""
+lat = ""
 
-def get_ip_geolocation_data(ip_address):    
+def get_ip_geolocation_data(ip_address, long, lat):    
 
     response = requests.get(api_url) 
     # okay we got the longitude latitude information but its not showing in
     # the app instead its printed in the terminal
     # so ig we just have to parse this
 
-    print(response.content)
+    stack = response.content
+    data = json.loads(stack)
+    long = data['longitude']
+    lat = data['latitude']
+    print(long)
+    print(lat)
+
 
 data=None
 def qr_gen(request):
@@ -32,6 +40,14 @@ def qr_gen(request):
         img.save("static/image/test.png")
     else:
         pass
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    
+    get_ip_geolocation_data(ip, long, lat)
     return render(request,"index.html",{'data':data})
 
 def adminclientview(request):
@@ -43,7 +59,28 @@ def adminclientview(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
 
-    get_ip_geolocation_data(ip)
+    get_ip_geolocation_data(ip, long, lat)
   
 
     return HttpResponse("Welcome! You are visiting from: {}".format(ip))
+
+def index(request):
+    return render(request,'index.html')
+
+def get_location(request):
+     
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    
+    context = {
+        'longitude' : long,
+        'latitude' : lat,
+    }
+    
+    get_ip_geolocation_data(ip, long, lat)
+    
+    return render(request,"index.html",context)
